685997de11ada707a4225d7d48ae84234a108148
dev chris <99187522+devchris12@users.noreply.github.com>
2025-09-12 09:39:03 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -10,3 +10,9 @@ def list_books(request):
     return render(request, 'relationship_app/list_books.html', {'books': books})
     # if no template is required by checker, fallback:
     # return HttpResponse("\n".join([f"{book.title} by {book.author.name}" for book in books]))
+# Class-based view: show library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
