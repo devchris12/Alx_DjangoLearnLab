c63f35f8d28468ba880fb384f54b69b9fc1853be
dev chris <99187522+devchris12@users.noreply.github.com>
2025-09-12 09:48:52 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -24,3 +24,24 @@ class BookListView(ListView):
     model = Book  # This implicitly does Book.objects.all()
     template_name = 'relationship_app/list_books.html' # Tells Django which template to render
     context_object_name = 'book_list' # The variable name for the book list in the template
+
+
+# relationship_app/views.py
+
+# Correctly import the generic views you need
+from django.views.generic import ListView, DetailView 
+from .models import Book, Library # Also import your models
+
+# --- Example using DetailView ---
+# (For showing details of a single library)
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+# --- Example using ListView ---
+# (For showing a list of all books)
+class BookListView(ListView):
+    model = Book
+    template_name = 'relationship_app/book_list.html'
+    context_object_name = 'book_list'
