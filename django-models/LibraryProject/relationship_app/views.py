1a8305a68bfa523abefce255c50814f87fa76da2
dev chris <99187522+devchris12@users.noreply.github.com>
2025-09-12 10:39:33 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -15,6 +15,31 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+
+# --- Role checks ---
+def is_admin(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
+
+def is_librarian(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
+
+def is_member(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Member"
+
+# --- Views ---
+@user_passes_test(is_admin, raise_exception=True)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+@user_passes_test(is_librarian, raise_exception=True)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+@user_passes_test(is_member, raise_exception=True)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
 
 # Register view
 def register(request):
