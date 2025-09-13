f78dc4a5bdc2717eb69b57d1e074aa5527f3f7e2
dev chris <99187522+devchris12@users.noreply.github.com>
2025-09-13 08:39:52 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -14,6 +14,31 @@ from django.views.generic import CreateView, DetailView, ListView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from .models import Book, Library
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+
+# Role check helpers
+def is_admin(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
+
+def is_librarian(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
+
+def is_member(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Member"
+
+# Views
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
 
 # Function-based view: list all books
 def list_books(request):
