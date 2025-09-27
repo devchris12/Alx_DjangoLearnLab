fa3a4266f561e7919267420f2bcf2f58a402ae9b
dev chris <99187522+devchris12@users.noreply.github.com>
2025-09-27 20:27:04 +0100

    Update views.py

--- advanced_api_project/api/views.py
+++ advanced_api_project/api/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 # views.py
+# views.py
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 class BookListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly]  # Allows read-only access to unauthenticated users
 
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly]  # Allows read-only access to unauthenticated users
