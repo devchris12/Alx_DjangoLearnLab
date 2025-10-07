39727ebeca2ebf04850b8a827f0aa33535b12d8a
dev chris <99187522+devchris12@users.noreply.github.com>
2025-10-07 12:36:34 +0100

    Update admin.py

--- social_media_api/accounts/admin.py
+++ social_media_api/accounts/admin.py
@@ -1,18 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import Profile
 
 
-@admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    """
-    Admin configuration for CustomUser model
-    """
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'created_at')
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'created_at')
-    search_fields = ('username', 'email', 'first_name', 'last_name')
-    ordering = ('-created_at',)
-    
-    fieldsets = UserAdmin.fieldsets + (
-        ('Additional Info', {'fields': ('bio', 'profile_picture', 'followers')}),
-    )
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'created_at']
+    search_fields = ['user__username', 'bio']
+    filter_horizontal = ['following']
