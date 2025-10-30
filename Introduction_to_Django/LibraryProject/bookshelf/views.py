adff83f1acab6e8bd3ac29b12f8c0c73204bf3a3
salome Ananah-obo <salomeobo10@gmail.com>
2025-10-30 15:57:52 +0100

    new folder

--- Introduction_to_Django/LibraryProject/bookshelf/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Book
-
-
-def book_list(request):
-    """View to display all books."""
-    books = Book.objects.all()
-    return render(request, 'bookshelf/book_list.html', {'books': books})
